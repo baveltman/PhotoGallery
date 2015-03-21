5c04bf63f789fe5674fec19d7a240313440c3517
baveltman <baveltman@gmail.com>
2015-03-21 18:17:29 -0500

    Fixed broken api call. Let's make sure to do these over https from now on

--- app/src/main/java/apps/baveltman/photogallery/FlickerFetcher.java
+++ app/src/main/java/apps/baveltman/photogallery/FlickerFetcher.java
@@ -1,5 +1,8 @@
 package apps.baveltman.photogallery;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,6 +13,13 @@ import java.net.URL;
  * A class to handle networking between the app and flicker
  */
 public class FlickerFetcher {
+
+    public static final String TAG = "FlickrFetchr";
+    private static final String ENDPOINT = "https://api.flickr.com/services/rest/";
+    private static final String API_KEY = "ddff3a61f833d31015b57ce80f300369";
+    private static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
+    private static final String PARAM_EXTRAS = "extras";
+    private static final String EXTRA_SMALL_URL = "url_s";
 
     /**
      * fetches raw data from a url and returns the data as a byte array
@@ -49,6 +59,25 @@ public class FlickerFetcher {
      */
     public String getUrl(String urlSpec) throws IOException {
         return new String(getUrlBytes(urlSpec));
+    }
+
+    /**
+     * creates request uri and executes api call
+     */
+    public void fetchItems() {
+        try {
+
+            String url = Uri.parse(ENDPOINT).buildUpon()
+                    .appendQueryParameter("method", METHOD_GET_RECENT)
+                    .appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter("format", "rest")
+                    .appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL).build().toString();
+
+            String xmlString = getUrl(url);
+            Log.i(TAG, "Received xml: " + xmlString);
+        } catch (IOException ioe) {
+            Log.e(TAG, "Failed to fetch items", ioe);
+        }
     }
 
 }
