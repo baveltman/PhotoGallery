66927cab3c9e332cae76d1324195f28a98599bf1
baveltman <baveltman@gmail.com>
2015-03-21 18:19:27 -0500

    Added class to encapsulate info from flickr api

---
+++ app/src/main/java/apps/baveltman/photogallery/GalleryItem.java
@@ -0,0 +1,15 @@
+package apps.baveltman.photogallery;
+
+/**
+ * Class to encapsulate photo info from Flickr
+ */
+public class GalleryItem {
+
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    public String toString() {
+        return mCaption;
+    }
+}
