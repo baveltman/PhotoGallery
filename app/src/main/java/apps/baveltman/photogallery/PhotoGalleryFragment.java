31043fa2a626d343e919f5d2dadbe48f8e629fa1
baveltman <baveltman@gmail.com>
2015-03-20 19:49:15 -0500

    Created nested class to facilitate work in a background thread of retriving info from the web

--- app/src/main/java/apps/baveltman/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/apps/baveltman/photogallery/PhotoGalleryFragment.java
@@ -1,16 +1,22 @@
 package apps.baveltman.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
+import java.io.IOException;
+
 /**
  * Created by borisvelt on 3/20/15.
  */
 public class PhotoGalleryFragment extends Fragment {
+
+    private static final String LOGGERTAG = "PhotoGalleryFragment";
 
     GridView mGridView;
 
@@ -18,6 +24,9 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+
+        //call on nested class to act in a background thread
+        new FetchItemsTask().execute();
     }
 
     @Override
@@ -28,5 +37,24 @@ public class PhotoGalleryFragment extends Fragment {
         mGridView = (GridView)v.findViewById(R.id.photo_gridview);
 
         return v;
+    }
+
+
+    /**
+     * nested class used to facilitate a background task of retrieving info from url
+     */
+    private class FetchItemsTask extends AsyncTask<Void,Void,Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickerFetcher().getUrl("http://www.flickr.com");
+                Log.i(LOGGERTAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe) {
+                Log.e(LOGGERTAG, "Failed to fetch URL: ", ioe);
+            }
+            return null;
+        }
+
     }
 }
