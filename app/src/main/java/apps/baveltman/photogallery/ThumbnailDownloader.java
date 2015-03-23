0b11680105941da791d126afcc81771453b2cae7
baveltman <baveltman@gmail.com>
2015-03-22 19:37:45 -0500

    Added background thread to fetch images by url one by one, pt2

--- app/src/main/java/apps/baveltman/photogallery/ThumbnailDownloader.java
+++ app/src/main/java/apps/baveltman/photogallery/ThumbnailDownloader.java
@@ -1,7 +1,17 @@
 package apps.baveltman.photogallery;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.Message;
 import android.util.Log;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by borisvelt on 3/22/15.
@@ -9,6 +19,13 @@ import android.util.Log;
 public class ThumbnailDownloader<Token> extends HandlerThread {
 
     private static final String TAG = "ThumbnailDownloader";
+    private static final int MESSAGE_DOWNLOAD = 0;
+
+    Handler mHandler;
+
+    Map<Token, String> requestMap =
+            Collections.synchronizedMap(new HashMap<Token, String>());
+
 
     public ThumbnailDownloader() {
         super(TAG);
@@ -16,5 +33,44 @@ public class ThumbnailDownloader<Token> extends HandlerThread {
 
     public void queueThumbnail(Token token, String url) {
         Log.i(TAG, "Got an URL: " + url);
+        requestMap.put(token, url);
+
+        mHandler.obtainMessage(MESSAGE_DOWNLOAD, token)
+                .sendToTarget();
+    }
+
+    @SuppressLint("HandlerLeak")
+    @Override
+    protected void onLooperPrepared() {
+        mHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == MESSAGE_DOWNLOAD) {
+                    @SuppressWarnings("unchecked")
+                    Token token = (Token)msg.obj;
+                    Log.i(TAG, "Got a request for url: " + requestMap.get(token));
+                    handleRequest(token);
+                }
+            }
+        };
+    }
+
+    private void handleRequest(final Token token) {
+        try {
+            final String url = requestMap.get(token);
+
+            if (url == null) {
+                return;
+            }
+
+            byte[] bitmapBytes = new FlickerFetcher().getUrlBytes(url);
+
+            final Bitmap bitmap = BitmapFactory
+                    .decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
+            Log.i(TAG, "Bitmap created");
+
+        } catch (IOException ioe) {
+            Log.e(TAG, "Error downloading image", ioe);
+        }
     }
 }
