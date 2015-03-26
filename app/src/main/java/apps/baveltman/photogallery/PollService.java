1bf09df3da52e6c79c79db82c86eb2877a36783c
baveltman <baveltman@gmail.com>
2015-03-25 21:31:03 -0500

    Created background service for app and declared it in Manifest

---
+++ app/src/main/java/apps/baveltman/photogallery/PollService.java
@@ -0,0 +1,22 @@
+package apps.baveltman.photogallery;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * background service for app
+ */
+public class PollService extends IntentService {
+
+    private static final String TAG = "PollService";
+
+    public PollService(){
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.i(TAG, "Received an intent: " + intent);
+    }
+}
