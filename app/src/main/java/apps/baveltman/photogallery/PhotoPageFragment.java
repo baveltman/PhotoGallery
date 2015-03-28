cb589e31a4f6e12356ed4a24f096bb85002a4186
baveltman <baveltman@gmail.com>
2015-03-28 16:48:52 -0500

    Added progressbar to webview

--- app/src/main/java/apps/baveltman/photogallery/PhotoPageFragment.java
+++ app/src/main/java/apps/baveltman/photogallery/PhotoPageFragment.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 /**
  * Fragment to control webview of single grid images
@@ -26,6 +29,10 @@ public class PhotoPageFragment extends VisibleFragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_photo_page, parent, false);
 
+        final ProgressBar progressBar = (ProgressBar)v.findViewById(R.id.photo_progressBar);
+        progressBar.setMax(100); // WebChromeClient reports in range 0-100
+        final TextView titleTextView = (TextView)v.findViewById(R.id.photo_textView);
+
         mWebView = (WebView)v.findViewById(R.id.photo_webview);
 
         //enable js for webview
@@ -36,6 +43,21 @@ public class PhotoPageFragment extends VisibleFragment {
                 return false;
             }
         });
+
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView webView, int progress){
+                    if (progress == 100) {
+                    progressBar.setVisibility(View.INVISIBLE);
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setProgress(progress);
+                }
+            }
+
+            public void onReceivedTitle(WebView webView, String title) {
+                    titleTextView.setText(title);
+                }
+            });
 
         mWebView.loadUrl(mUrl);
 
